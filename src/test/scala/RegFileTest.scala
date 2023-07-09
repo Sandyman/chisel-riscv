f7d517f6de65b4181ed0aead51d19edffef907ae
Sander Huijsen <sander.huijsen@gmail.com>
2023-07-09 18:23:32 +0800

    Refactored unit tests a bit.

--- src/test/scala/RegFileTest.scala
+++ src/test/scala/RegFileTest.scala
@@ -5,18 +5,41 @@ import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
 class BasicTest extends AnyFlatSpec with ChiselScalatestTester {
-    it should "Write and read back random values" in {
-        val m = 32
-        val n = 32
-        val rnd = new scala.util.Random
-        test(new RegFile(m, n)) { r => 
+    val m = 32
+    val n = 32
+    it should "be initialised with all 0s" in {
+        test(new RegFile(m, n)) { r =>
             for (t <- 0 until m) {
                 // Check that initialised value is 0
                 r.io.rdAddr1.poke(t)
                 r.io.rdAddr2.poke(t)
                 r.io.rdData1.expect(0)
                 r.io.rdData2.expect(0)
+            }
+        }
+    }
+    it should "always return 0 for register address 0" in {
+        test(new RegFile(m, n)) { r =>
+            // Write a non-0 value into register 0
+            r.io.wen.poke(1)
+            r.io.wrAddr.poke(0)
+            r.io.wrData.poke(0x7fffffff)
 
+            // Progress time
+            r.clock.step(1)
+
+            // Check that both outputs return 0 for address 0
+            r.io.wen.poke(0)
+            r.io.rdAddr1.poke(0)
+            r.io.rdAddr2.poke(0)
+            r.io.rdData1.expect(0)
+            r.io.rdData2.expect(0)
+        }
+    }
+    it should "write and read back random values" in {
+        val rnd = new scala.util.Random
+        test(new RegFile(m, n)) { r =>
+            for (t <- 1 until m) {
                 // Create some random value
                 val num = rnd.nextInt((1 << (n - 1)) - 1)
 
@@ -32,14 +55,8 @@ class BasicTest extends AnyFlatSpec with ChiselScalatestTester {
                 r.io.wen.poke(0)
                 r.io.rdAddr1.poke(t)
                 r.io.rdAddr2.poke(t)
-                if (t == 0) {
-                    // But r0 always returns 0!
-                    r.io.rdData1.expect(0)
-                    r.io.rdData2.expect(0)
-                } else {
-                    r.io.rdData1.expect(num)
-                    r.io.rdData2.expect(num)
-                }
+                r.io.rdData1.expect(num)
+                r.io.rdData2.expect(num)
             }
         }
     }
