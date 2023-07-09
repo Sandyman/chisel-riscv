b89de2dfc1dc695e4bb8cd6493d00e9149061829
Sander Huijsen <sander.huijsen@gmail.com>
2023-07-09 18:11:48 +0800

    Refactored unit test to not use iotesters any more.

--- src/test/scala/RegFileTest.scala
+++ src/test/scala/RegFileTest.scala
@@ -1,38 +1,46 @@
 package problems
 
-import chisel3.iotesters.PeekPokeTester
+import chisel3._
+import chiseltest._
+import org.scalatest.flatspec.AnyFlatSpec
 
-class RegFileTest(r: RegFile, m: Int, n: Int) extends PeekPokeTester(r) {
+class BasicTest extends AnyFlatSpec with ChiselScalatestTester {
+    it should "Write and read back random values" in {
+        val m = 32
+        val n = 32
+        val rnd = new scala.util.Random
+        test(new RegFile(m, n)) { r => 
+            for (t <- 0 until m) {
+                // Check that initialised value is 0
+                r.io.rdAddr1.poke(t)
+                r.io.rdAddr2.poke(t)
+                r.io.rdData1.expect(0)
+                r.io.rdData2.expect(0)
 
-    for (t <- 0 until m) {
-        // Check that initialised value is 0
-        poke(r.io.rdAddr1, t)
-        poke(r.io.rdAddr2, t)
-        expect(r.io.rdData1, 0)
-        expect(r.io.rdData2, 0)
+                // Create some random value
+                val num = rnd.nextInt((1 << (n - 1)) - 1)
 
-        // Create some random value
-        val num = rnd.nextInt((1 << (n - 1)) - 1)
+                // And write to register        
+                r.io.wen.poke(1)
+                r.io.wrAddr.poke(t)
+                r.io.wrData.poke(num)
 
-        // And write to register        
-        poke(r.io.wen, 1)
-        poke(r.io.wrAddr, t)
-        poke(r.io.wrData, num)
+                // Ensure write actually happens
+                r.clock.step(1)
 
-        // Ensure write actually happens
-        step(1)
-
-        // Read back and check it is what we expect
-        poke(r.io.wen, 0)
-        poke(r.io.rdAddr1, t)
-        poke(r.io.rdAddr2, t)
-        if (t == 0) {
-            // But r0 always returns 0!
-            expect(r.io.rdData1, 0)
-            expect(r.io.rdData2, 0)
-        } else {
-            expect(r.io.rdData1, num)
-            expect(r.io.rdData2, num)
+                // Read back and check it is what we expect
+                r.io.wen.poke(0)
+                r.io.rdAddr1.poke(t)
+                r.io.rdAddr2.poke(t)
+                if (t == 0) {
+                    // But r0 always returns 0!
+                    r.io.rdData1.expect(0)
+                    r.io.rdData2.expect(0)
+                } else {
+                    r.io.rdData1.expect(num)
+                    r.io.rdData2.expect(num)
+                }
+            }
         }
     }
 }
