0965ac437128751a565da481d938bc5e94555bd9
Sander Huijsen <sander.huijsen@gmail.com>
2023-07-19 20:46:25 +0800

    Added ImmGen class for immediate value generation.

--- src/main/scala/ImmGen.scala
+++ src/main/scala/ImmGen.scala
@@ -2,3 +2,45 @@ import chisel3._
 import chisel3.util._
 import chisel3.experimental._
 
+object ImmType {
+    val IImm     = 0.U(3.W)
+    val SImm     = 1.U(3.W)
+    val BImm     = 2.U(3.W)
+    val UImm     = 3.U(3.W)
+    val JImm     = 4.U(3.W)
+}
+
+import ImmType._
+
+class ImmGenIO(xlen: Int) extends Bundle {
+    val inst = Input(UInt(xlen.W))
+    val sel = Input(UInt(3.W))
+    val imm = Output(UInt(xlen.W))
+}
+
+trait ImmGen extends Module {
+    def xlen: Int
+    val io: ImmGenIO
+}
+
+class ImmGenSimple (val xlen: Int) extends ImmGen {
+    val io = IO(new ImmGenIO(xlen))
+
+    val imm_i: UInt => UInt = a => Cat(Fill(21, a(31)), a(30, 20))
+    val imm_s: UInt => UInt = a => Cat(Fill(21, a(31)), a(30, 25), a(11, 7))
+    val imm_b: UInt => UInt = a => Cat(Fill(20, a(31)), a(7), a(30, 25), a(11, 8), 0.B)
+    val imm_u: UInt => UInt = a => Cat(a(31, 12), Fill(12, 0.B))
+    val imm_j: UInt => UInt = a => Cat(Fill(12, a(31)), a(19, 12), a(20), a(30, 21), 0.B) 
+
+    io.imm := MuxLookup(
+        0.U,
+        io.sel,
+        Seq(
+            IImm -> imm_i(io.inst),
+            SImm -> imm_s(io.inst),
+            BImm -> imm_b(io.inst),
+            UImm -> imm_u(io.inst),
+            JImm -> imm_j(io.inst),
+        )
+    )
+}
--- src/test/scala/ImmGenTest.scala
+++ src/test/scala/ImmGenTest.scala
@@ -1,3 +1,45 @@
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
+
+import ImmType._
+
+class BasicImmGenTest extends AnyFlatSpec with ChiselScalatestTester {
+    val m = 32 // Number of registers
+    val xlen = 32 // Width of register in bits
+    it should "build UInt of value 0 from 12-bit 0 value" in {
+        test(new ImmGenSimple(xlen)) { r => 
+            r.io.sel.poke(IImm)
+            r.io.inst.poke(0 << 20)
+            r.io.imm.expect(0)
+        }
+    }
+    it should "build UInts from 12-bit a positive value" in {
+        test(new ImmGenSimple(xlen)) { r =>
+            r.io.sel.poke(IImm)
+            r.io.inst.poke(12 << 20)
+            r.io.imm.expect(12)
+        }
+    }
+    it should "build UInts from the biggest positive value" in {
+        test(new ImmGenSimple(xlen)) { r =>
+            r.io.sel.poke(IImm)
+            r.io.inst.poke(0x7ff << 20)
+            r.io.imm.expect(0x7ff)
+        }
+    }
+    it should "build UInt from a negative 12-bit value" in {
+        test(new ImmGenSimple(xlen)) { r => 
+            r.io.sel.poke(IImm)
+            r.io.inst.poke("h8000_0000".U)
+            r.io.imm.expect("hffff_f800".U)
+        }
+    }
+    it should "built UInt of value -1 from 12-bit -1 value" in {
+        test(new ImmGenSimple(xlen)) { r =>
+            r.io.sel.poke(IImm)
+            r.io.inst.poke("hfff0_0000".U)
+            r.io.imm.expect("hffff_ffff".U)
+        }
+    }
+}
