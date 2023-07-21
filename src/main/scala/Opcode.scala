49b9e5c98e166d9cacbe3fcb9936ca92ef1d7fb1
Sander Huijsen <sander.huijsen@gmail.com>
2023-07-21 22:28:34 +0800

    Fixed Opcode.scala.

--- src/main/scala/Opcode.scala
+++ src/main/scala/Opcode.scala
@@ -3,15 +3,15 @@ import chisel3.util._
 import chisel3.experimental._
 
 object Opcode {
-    val LUI      = "b0110111".U,
-    val AUIPC    = "b0010111".U,
-    val JAL      = "b1101111".U,
-    val JALR     = "b1100111".U,
-    val BRANCH   = "b1100011".U,
-    val LOAD     = "b0000011".U,
-    val STORE    = "b0100011".U,
-    val OPIMM    = "b0010011".U,
-    val OP       = "b0110011".U,
-    val MISCMEM  = "b0001111".U,
-    val SYSTEM   = "b1110011".U,
+    val LUI      = "b0110111".U(7.W)
+    val AUIPC    = "b0010111".U(7.W)
+    val JAL      = "b1101111".U(7.W)
+    val JALR     = "b1100111".U(7.W)
+    val BRANCH   = "b1100011".U(7.W)
+    val LOAD     = "b0000011".U(7.W)
+    val STORE    = "b0100011".U(7.W)
+    val OPIMM    = "b0010011".U(7.W)
+    val OP       = "b0110011".U(7.W)
+    val MISCMEM  = "b0001111".U(7.W)
+    val SYSTEM   = "b1110011".U(7.W)
 }
