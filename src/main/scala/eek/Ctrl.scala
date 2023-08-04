48854f201c23e5d36fba4099dd0ce1b50ae281c9
Sander Huijsen <sander.huijsen@gmail.com>
2023-08-04 18:10:08 +0800

    Started on the Control subsystem.

---
+++ src/main/scala/eek/Ctrl.scala
@@ -0,0 +1,22 @@
+package eek
+
+import chisel3._
+import chisel3.stage._
+import chisel3.util.log2Ceil
+
+class CtrlIO(xlen: Int) extends Bundle {
+}
+
+trait Ctrl extends Module {
+    def xlen: Int
+    val io: CtrlIO
+}
+
+class SimpleCtrl(val xlen: Int) extends Ctrl {
+    val io = IO(new CtrlIO(xlen))
+}
+
+object CtrlDriver extends App {
+    val xlen = 32
+    (new ChiselStage).emitVerilog(new SimpleCtrl(xlen), args)
+}
